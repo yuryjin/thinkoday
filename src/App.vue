86b5db34a97a39a1f32be56519d0b3eaf5efa290
yuryjin <48969408+yuryjin@users.noreply.github.com>
2021-01-11 22:08:42 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Thinkoday - quote of the day!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+//import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+//Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+//Vue.use(IconsPlugin)
+
+createApp(App).mount('#app')
